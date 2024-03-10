f96ee7f0514dbeba07fac49145af4be32c86f58e
Sharon K <shapzmeloz@gmail.com>
2024-03-10 14:19:48 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+
+message = f"Hello {name}, you are {age} years old and live in {location}."
+
+print(message)
